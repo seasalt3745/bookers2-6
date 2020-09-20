4cf3c616cdaf60aaa76ef5f6160a9379a637eb44
vagrant <vagrant@localhost.localdomain>
2020-09-20 04:23:00 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,22 @@
 class BooksController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :user_check, only: [:edit, :update]
+
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @booker = Book.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -33,16 +40,23 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body,:user_id)
+  end
+
+  def user_check
+    book = Book.find(params[:id])
+    if current_user.id != book.user_id
+      redirect_to books_path
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  belongs_to :books
+  has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
+  validates :introduction, length: { maximum: 50 }
 end
