836c35b1767bd802c19ee6bf7758e85815f9d50e
vagrant <vagrant@localhost.localdomain>
2020-09-21 15:44:56 +0000

    [Add]a

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,7 @@ class FavoritesController < ApplicationController
       # favorite = Favorite.new(book_id: book.id)
 	  # favorite.user_id = current_user.id
     favorite.save
-    redirect_to redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
 
@@ -14,7 +14,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
 end
